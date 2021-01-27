6d4eae902f42e8bd8e1f63dcdd5446af5bc2058d
Monisha Baskaran <baskarma@users.noreply.github.com>
2021-01-26 22:49:20 -0500

    Solution 6 - M

---
+++ Arrays and Strings/Solutions-M/Solution6M.java
@@ -0,0 +1,38 @@
+/* 
+ * Problem Statement:
+ * Implement a method to perform basic string compression using the counts of repeated characters.
+ * For example, the string aabcccccaaa would become a2b1c5a3.
+ * If the "compressed" string would not become smaller than the original string, your method should return the original string.
+ */
+
+public class Solution6M {
+
+    private static String CompressString(String input){
+        if(input.isEmpty() || input == null)
+            return null;
+
+        StringBuilder sb = new StringBuilder();
+        int charCount = 1;
+
+        for(int i = 0; i<input.length();i++){
+            if((i+1 < input.length()) && (input.charAt(i) == input.charAt(i+1))){
+                charCount++;
+            }
+            else{
+                sb.append(input.charAt(i));
+                sb.append(charCount);
+                charCount = 1;
+            }
+        }
+        return sb.toString();
+    }
+}
+
+/* 
+ * Testcases:
+ * 
+ * T1: input = "aabccccaaa" --> "a2b1c4a3"
+ * T2: input = "   " --> " 3"
+ * T3: input = "" --> null
+ * T4: input = "1111ffrgyhst" --> "14f2r1g1y1h1s1t1"
+ */
