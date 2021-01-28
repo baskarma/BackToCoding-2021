b5989a2a5afe0935c3bbb4de48a5fe91bc19c7ca
Monisha Baskaran <baskarma@users.noreply.github.com>
2021-01-28 01:08:52 -0500

    Solution 7 - M

--- Arrays and Strings/Solutions-M/Solution7M.java
+++ Arrays and Strings/Solutions-M/Solution7M.java
@@ -5,17 +5,63 @@
  */
 public class Solution7M {
     public static void main(String[] args){
-        int[][] matrix = {{1,1,1},{2,2,2},{3,3,3}};
-        System.out.println(RotateMatrix(matrix));
+        int[][] matrix = {{1,1,1,1},{2,2,2,2},{3,3,3,3}};
+        int[][] rotatedMatrix = RotateMatrix(matrix, matrix.length);
+        int[][] rotatedInPlaceMatrix = RotateMatrixInPlace(matrix, matrix.length);
+        PrintMatrix(rotatedMatrix);
+        System.out.println();
+        PrintMatrix(rotatedInPlaceMatrix);
+    }
+    //print the matrix
+    private static void PrintMatrix(int[][] matrix){
+        for(int i = 0 ; i < matrix.length; i++){
+            System.out.println();
+            for(int j = 0; j < matrix.length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+    
+    //Rotate Matrix using new Matrix
+    private static int[][] RotateMatrix(int[][] matrix, int n){
+        if(matrix.length != matrix[0].length || matrix == null)
+            return matrix;
+        
+        int[][] rotatedMatrix = new int[n][n];
+        for(int i = 0 ; i < n; i++){
+            int k = n - 1;
+            for(int j = 0; j < n; j++){
+                rotatedMatrix[i][j] = matrix[k][i];
+                k--;
+            }
+        }
+        return rotatedMatrix;
     }
 
-    private static int[][] RotateMatrix(int[][] matrix){
+    //Rotate Matrix in place by swapping values
+    private static int[][] RotateMatrixInPlace(int[][] matrix, int n){
         if(matrix.length != matrix[0].length)
             return matrix;
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j > matrix.length; j--){
+        for(int i = 0; i < n/2; i++)
+        {
+            //stepping from outside to inside of the matrix
+            int firstRow = i;
+            int lastRow = n - i - 1;
+
+            for(int j = firstRow; j < lastRow; j++)
+            {
+                int offset = j - firstRow;
+
+                int temp = matrix[firstRow][j]; // save top to temp
+
+                matrix[firstRow][j] = matrix[lastRow - offset][firstRow]; //swap left to top
                 
+                matrix[lastRow - offset][firstRow] = matrix[lastRow][lastRow - offset]; // swap bottom to left
+                
+                matrix[lastRow][lastRow - offset] = matrix[j][lastRow]; // swap right to bottom
+                
+                matrix[j][lastRow] = temp; // swap temp to top
             }
         }
         return matrix;
