d96713af27bf4a2816061122afb4b1a029014ed2
Monisha Baskaran <baskarma@users.noreply.github.com>
2021-01-30 23:08:19 -0500

    In progress

---
+++ Arrays and Strings/Solutions-M/Solution15M.java
@@ -0,0 +1,17 @@
+/* 
+ * Problem Statement:
+ * There are three types of edits that can be performed on strings : 
+ * insert, 
+ * remove 
+ * or replace a character.
+ * Given two strings, write a function to check if they are one edit( or zero edits) away.
+ */
+public class Solution15M {
+   public static void main(String[] args){
+
+   } 
+
+   private static boolean isOneEditAway(String s1, String s2){
+       
+   }
+}
